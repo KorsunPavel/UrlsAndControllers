551288dc48424d47226639125047cb08a828a1af
KorsunPavel <pavel.korsun1@gmail.com>
2017-08-29 10:40:10 +0300

    changes in home controller

--- ControllersAndActions2/Controllers/HomeController.cs
+++ ControllersAndActions2/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ControllersAndActions2.Infrastructure;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -15,6 +16,19 @@ namespace ControllersAndActions2.Controllers
 
             return View("MyAction");
             
+        }
+
+        public ActionResult ProduceOutput()
+        {
+            if (Server.MachineName == "ZEBRA-0601")
+            {
+                return new CustomRedirectResult { Url = "/Home/Index" };
+            }
+            else
+            {
+                Response.Write("Controller: Derived, Action: ProduceOutput");
+                return null;
+            }
         }
     }
 }
