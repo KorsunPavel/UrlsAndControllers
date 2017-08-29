888f79435cff69b5283cdf965fbbb0aa6749595b
KorsunPavel <pavel.korsun1@gmail.com>
2017-08-29 08:39:31 +0300

    initial commit

---
+++ ControllersAndActions2.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ControllersAndActions2.Tests {
+    [TestClass]
+    public class UnitTest1 {
+        [TestMethod]
+        public void TestMethod1() {
+        }
+    }
+}
